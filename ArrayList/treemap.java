edcc347b8636efffefa8430b9ada89e6f8639153
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2026-03-04 09:48:47 +0200

    Create treemap.java

---
+++ ArrayList/treemap.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.util.Decimal;
+
+public class Main {
+       public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String input = scanner.nextLine();
+
+        List<Double> degrees = input.split(" ");
+
+        TreeSet<String, List<Double>> = String => List<Double>>;
+
+        for (String double : numbers) {
+
+        }
+
+        for (TreeMap.Entry<String Double> entry : countMap.entrySet()) {
+            System.out.println(df.format(entry.getKey() + " " + entry.getValue());
+        }
+
+        scanner.close();
+     }
+}
